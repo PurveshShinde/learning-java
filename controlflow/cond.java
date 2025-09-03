42d8bd755a031952fcbb6fe6203120dc4a82ec28
Purvesh <shindepurvesh007@gmail.com>
2025-09-03 13:09:52 +0530

    if else statements

---
+++ controlflow/cond.java
@@ -0,0 +1,10 @@
+
+class cond {
+
+    public static void main(String[] args) {
+        int oats = 2;
+        if (oats > 0);
+        System.out.println("Let's make oatmeal today!");
+
+    }
+}
---
+++ variables/numbers.java
@@ -0,0 +1,15 @@
+
+class numbers {
+
+    public static void main(String[] args) {
+        // Write your code here 💖
+        int livingRoom = 15;
+        int masterBedroom = 45;
+        int bathroom = 10;
+        //result
+        System.out.println(livingRoom);
+        System.out.println(masterBedroom);
+        System.out.println(bathroom);
+
+    }
+}
