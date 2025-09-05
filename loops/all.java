64dd300249a7e68b9fda16f6a97fd111f88651f1
Purvesh <shindepurvesh007@gmail.com>
2025-09-05 15:29:45 +0530

    leanred and implimented loops in real time

---
+++ loops/all.java
@@ -0,0 +1,14 @@
+
+public class all {
+
+    public static void main(String[] args) {
+        // Write your code here 💖
+        int i = 1;
+
+        while (i < 15) {
+            System.out.println("Line " + i + ": LET'S GO Avengers!");
+            i = i + 1;
+        }
+
+    }
+}
---
+++ loops/loop.java
@@ -0,0 +1,13 @@
+
+public class loop {
+
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 5) {
+            System.out.println(num);
+            num = num + 1;
+
+        }
+    }
+}
