6612eb56f47a3c6fbe93e507688d3d5c8b29b271
Purvesh <shindepurvesh007@gmail.com>
2025-09-06 16:13:16 +0530

    Learned about infinite loop in Java

---
+++ loops/For.java
@@ -0,0 +1,13 @@
+
+class For {
+
+    public static void main(String[] args) {
+        // Write your code here 💖
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println("I am a great at programmer! 🚀");
+
+        }
+
+    }
+}
