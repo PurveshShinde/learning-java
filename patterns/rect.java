d4f38cdf709d0d25b9af9f0a1b32c448219d3c14
Purvesh <shindepurvesh007@gmail.com>
2025-09-16 10:30:44 +0530

    hollow rectangle pattern

--- patterns/rect.java
+++ patterns/rect.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 class rect {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
 
 }
