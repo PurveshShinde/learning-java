fcfe27aae3a8544d4c73e41b5e68396dcc533ff7
Purvesh <shindepurvesh007@gmail.com>
2025-09-27 19:02:01 +0530

    reverse triangle complete

--- patterns/p4.java
+++ patterns/p4.java
@@ -1,12 +1,10 @@
-public class p3 {
+public class p4 {
     public static void main(String args[]) {
-        int r = 5;
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
                 System.out.print("*");
             }
             System.out.println(" ");
         }
     }
-
 }
