804713b77050238bcd8eceac29b8d11e2ab16b9a
Purvesh <shindepurvesh007@gmail.com>
2025-12-04 16:35:47 +0530

    add DataDemo class with basic variable demonstration and output

---
+++ Variables/DataDemo.java
@@ -0,0 +1,14 @@
+public class DataDemo {
+    public static void main(String args[]){
+        String name = "Purvesh Shinde";
+        int age = 22;
+        float height = 5.5f;
+        boolean likesCoding = true;
+        System.out.println("name: " + name);
+        System.out.println("age: " +age);
+        System.out.println("height: " +height);
+        System.out.println("likes coding: " +likesCoding);
+
+    }
+    
+}
