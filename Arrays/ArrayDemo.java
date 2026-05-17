acafe6412968a9d6ad795a3a6f19c80e23ca79aa
Purvesh <shindepurvesh007@gmail.com>
2026-05-17 21:24:54 +0530

    Refactor ArrayDemo for improved readability and add rectangle, hollow, triangle, and triangleReverse pattern classes

--- Arrays/ArrayDemo.java
+++ Arrays/ArrayDemo.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
+
+import java.util.*;
+
 public class ArrayDemo {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Enter size : ");
         int size = sc.nextInt();
 
-        int [] arr = new int[size]; // Create an array of given size
-       
-        System.out.println("Enter elements :"); 
-        for(int i = 0; i< size; i++){ // Input Loop
+        int[] arr = new int[size]; // Create an array of given size
+
+        System.out.println("Enter elements :");
+        for (int i = 0; i < size; i++) { // Input Loop
             arr[i] = sc.nextInt();
         }
         System.out.println("Output : ");
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             System.out.print(arr[i] + " ");
         }
-         
-        
+
     }
 }
--- Patterns/hollow.java
+++ Patterns/hollow.java
@@ -1,10 +1,9 @@
 
 import java.util.Scanner;
 
-public class rectangle {
+public class hollow {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         int r = sc.nextInt();
@@ -12,9 +11,17 @@ public class rectangle {
 
         for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= c; j++) {
-                System.out.print("*");
+
+                if (i == 1 || i == r || j == 1 || j == c) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
             }
-            System.out.println("*");
+            System.out.println();
+
         }
     }
+
 }
